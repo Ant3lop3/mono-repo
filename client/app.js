802bfb13d9f06b5840c8350582fe1cfcc7240488
JOE WINTER <joewint3r@gmail.com>
2024-09-05 09:59:29 +0100

    major update, fully working live feed message board using a vite package client, express package server and supabase db table.

--- client/app.js
+++ client/app.js
@@ -1,1 +1,38 @@
-console.log("testing 1, 2, 3")
+//! for GET requests from db
+const moviessongsConstainer = document.getElementById("moviessongsCont");
+
+async function getMoviessongs() {
+    const response = await fetch("http://localhost:8080/moviessongs");
+    const data = await response.json()
+    console.log(data);
+    moviessongsConstainer.innerHTML = "";
+    data.forEach((moviessongs) => {
+        const p = document.createElement("p");
+        p.textContent = `My top movie is ${moviessongs.movie}, and top song is ${moviessongs.song}`;
+        moviessongsConstainer.appendChild(p)
+    });
+}
+
+getMoviessongs();
+
+//! for POST requests to db
+const form = document.getElementById("moviesSongsForm");
+
+async function handlePostMoviessongs(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form)
+    const data = Object.fromEntries(formData)
+    console.log(data)
+    await fetch("http://localhost:8080/moviessongs", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    form.reset();
+    getMoviessongs()
+}
+form.addEventListener("submit", handlePostMoviessongs)
+
--- server/server.js
+++ server/server.js
@@ -1,14 +1,44 @@
+//! Import your installed npm packages:
 import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
 
+//! set the server up by then enabling the use of the packages we installed
 const app = express();
-
 app.use(express.json());
+app.use(cors());
+dotenv.config();
 
-app.get("/", function (request, response){
-    response.json("You are looking at my root route, how roude");
-})
-// below starts the server:
-app.listen(8080, function() {
-    console.log("server is listening on port 8080")
-})
+//! Connect to our Db:
+const db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+//! start the server:
+app.listen(8080, function(){
+    console.log("app running on 8080")
+});
+
+//!make a GET and POST endpoint
+app.get("/", (request, response) => {
+    response.json("Welcome to the home route, now get out");
+});
+
+//! make an endpoint for the db using the db name
+app.get("/moviessongs", async (request, response) => {
+    const moviessongs = await db.query("SELECT * FROM moviessongs")
+    response.json(moviessongs.rows);
+});
+
+app.post("/moviessongs", async (request, response) => {
+    const movie = request.body.movie;
+    const song = request.body.song;
+    const newMoviessongs = await db.query(
+        "INSERT INTO moviessongs (movie, song) VALUES ($1, $2)",
+        [movie, song]
+    )
+    response.json(newMoviessongs);
+});
+
 
